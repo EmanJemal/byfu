0b11a02a48b4905433eacaba4e4c8860683d8527
Arafat Mohammed <arafatmohammedbusi@gmail.com>
2025-06-25 17:34:23 +0300

    commit

--- common/common.js
+++ common/common.js
@@ -17,4 +17,10 @@ document.getElementById("logout-btn").addEventListener("click", () => {
     localStorage.removeItem("sessionStart");  // 🧼 Clear the session
     window.location.href = "../index.html";   // 🔁 Redirect to login
   });
-  
+
+  const expectedKey = "8797254sa4d864fasdf1ads65f4652cas9+d7v41sad67vf48d5v6ss0d2v4as6d53v28d+f41sd984v56sd+v9+85sda-9dsv*/ds-";
+  const currentKey = localStorage.getItem("deviceKey");
+
+  if (currentKey !== expectedKey) {
+    document.body.innerHTML = "⛔ Unauthorized device. Access denied";
+}
